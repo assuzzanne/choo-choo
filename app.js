82e89ee416b42541cbb73f2941a78e582f2a8684
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-09-25 16:27:57 +0200

    feat(search): add new endpoint to api

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
+// const csv = require("csv-parser");
+// const fs = require("fs");
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+app.get("/status", (req, res) => {
+  res.status(200).send("OK!");
 });
 
-app.get('/status', (req, res) => {
-    res.status(200).send("OK!");
+app.get("/trains/:date", (req, res) => {
+  // '2011-12-03 T 10:15:30+01:00'
 
-  });
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on ${port}!`)
+  console.log(`Server is running on ${port}!`);
 });
 
 module.exports = app;
