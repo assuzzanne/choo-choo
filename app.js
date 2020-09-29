8672d375b8664d5b185ec6956536388a3abfbfb6
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-09-29 17:44:29 +0200

    feat(search): return trains that fit the date given as a param

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const app = express();
-
-// const csv = require("csv-parser");
-// const fs = require("fs");
+const csv = require("csv-parser");
+const fs = require("fs");
+const moment = require("moment");
 
 const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
 
 app.get("/status", (req, res) => {
   res.status(200).send("OK!");
 });
 
 app.get("/trains/:date", (req, res) => {
-  // '2011-12-03 T 10:15:30+01:00'
+  // {"date":"2011-12-03T10:15:30+01:00"}
+  // using the moment lib to convert the date in utc
+  // and format it in hours and minutes to compare it to the times from the csv file
+  const UTCTimeFormatted = moment(req.params.date).utc().format("HH:mm").toString();
 
+  const csvData = [];
+  fs.createReadStream("data.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+      csvData.push(row);
+    })
+    .on("end", () => {
+      console.info("CSV file successfully processed");
+      const result = csvData.filter((elem) => elem.time > UTCTimeFormatted)
+      res.status(200).send(result.slice(0, 3));
+    });
 });
 
 app.listen(port, () => {
