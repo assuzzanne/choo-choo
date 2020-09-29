1fd0f5a0d86645805a1e5178631dd53d3f0e5603
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-09-29 18:27:08 +0200

    chore(test): add unit test for success case

--- tests/app.test.js
+++ tests/app.test.js
@@ -8,14 +8,27 @@ chai.use(chaiHttp);
 describe("routing", () => {
   describe("/GET status route", () => {
     it("it should succeed", async () => {
-      const res = await chai
-      .request(app)
-      .get(
-        "/status/")
-      .send();
+      const res = await chai.request(app).get("/status/").send();
 
       chai.assert.equal(res.status, 200);
       chai.assert.deepEqual(res.text, "OK!");
+    });
+  });
+
+  describe("/GET trains route", () => {
+    it("it should succeed", async () => {
+      const date = "2011-12-03T10:15:30+01:00";
+      const res = await chai
+        .request(app)
+        .get("/trains/" + date)
+        .send();
+
+      chai.assert.equal(res.status, 200);
+      chai.assert.deepEqual(res.body, [
+        { time: "10:10", trainID: " Train-m02" },
+        { time: "11:30", trainID: " Train-m03" },
+        { time: "12:50", trainID: " Train-n01" },
+      ]);
     });
   });
 });
